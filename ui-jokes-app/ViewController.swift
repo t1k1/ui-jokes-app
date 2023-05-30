37eeb7ec9e85e8f93dd6e9be1dbeb2ec2bcf1e45
Aleksei Kolesnikov <ancientt1k1@gmail.com>
2023-05-30 14:04:04 +0300

    Initial Commit

---
+++ ui-jokes-app/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  ui-jokes-app
+//
+//  Created by Aleksey Kolesnikov on 03.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        textView.setup()
+    }
+
+
+}
+
+
+extension UITextView {
+    func setup() {
+        let fittingSize = CGSize(width: bounds.width, height: CGFloat.greatestFiniteMagnitude)
+        let size = sizeThatFits(fittingSize)
+        let topOffset = (bounds.size.height - size.height * zoomScale) / 2
+        let positiveTopOffset = max(1, topOffset)
+        contentOffset.y = -positiveTopOffset
+    
+        addBordersWithColor(color: UIColor.black, height: CGFloat(3))
+    }
+    
+    func addBordersWithColor(color: UIColor, height: CGFloat) {
+        let borderBot = UIView()
+        borderBot.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        borderBot.frame = CGRect(x: self.frame.origin.x,
+               y: self.frame.origin.y+self.frame.height-height,
+               width: self.frame.width,
+               height: height)
+        borderBot.backgroundColor = color
+        self.superview!.insertSubview(borderBot, aboveSubview: self)
+        let borderTop = UIView()
+        borderTop.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        borderTop.frame = CGRect(x: self.frame.origin.x,
+                                 y: self.frame.origin.y,
+                                 width: self.frame.width,
+                                 height: height)
+        borderTop.backgroundColor = color
+        self.superview!.insertSubview(borderTop, aboveSubview: self)
+    }
+}
---
+++ ui-jokes-appUITests/ui_jokes_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ui_jokes_appUITestsLaunchTests.swift
+//  ui-jokes-appUITests
+//
+//  Created by Aleksey Kolesnikov on 03.05.2023.
+//
+
+import XCTest
+
+final class ui_jokes_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
